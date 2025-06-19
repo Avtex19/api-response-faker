764ec5b0166c7d2538281c607a3c9fa2173aef8e
Avtandil Beradze <atukaberadze19@gmail.com>
2025-06-19 17:13:42 +0400

    use cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import rulesRouter from "./routes/RulesRouter";
-
+import cors from 'cors'
 
 const app = express()
-app.use(express.json())
-
+app.use(express.json());
+app.use(cors())
 app.use('/rules', rulesRouter)
 const PORT = 3000;
 
