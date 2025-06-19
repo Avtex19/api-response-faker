a1e52015b6ee23b787f44e971368ae1414fda363
Avtandil Beradze <atukaberadze19@gmail.com>
2025-06-19 14:27:19 +0400

    show create form conditionally

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,16 @@
+import Header from "./components/Header.tsx";
+import {useState} from "react";
+import {RulesForm} from "./components/rules/RulesForm.tsx";
 
 function App() {
+    const [rulesView, setRulesView] = useState<"CREATE" | "PREVIEW">("CREATE");
 
-  return (<>
-
-    </>
+    return (<div className={'w-full'}>
+            <Header rulesView={rulesView} setRulesView={setRulesView} />
+            {
+                rulesView === "CREATE" && <RulesForm/>
+            }
+        </div>
   )
 }
 
