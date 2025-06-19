f944f3f8ad5e9101ebd67ffe1efccd031b0a18a9
Avtandil Beradze <atukaberadze19@gmail.com>
2025-06-19 18:27:16 +0400

    update row content size to make json visible

--- front-end/src/components/rules/RulesTable.tsx
+++ front-end/src/components/rules/RulesTable.tsx
@@ -2,8 +2,10 @@ import {useEffect, useState} from "react";
 import fakeAxios from "../../../axios.ts";
 import type {IFakerRuleForm} from "./FakeRuleModel/FakeRuleForm.ts";
 import {ClipLoader} from "react-spinners";
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid, type GridRenderCellParams} from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper'
+import {JsonEditor} from "json-edit-react";
+
 
 export const RulesTable = () => {
     const [rules, setRules] = useState<IFakerRuleForm[]>([])
@@ -36,14 +38,33 @@ export const RulesTable = () => {
 
     return rules.length === 0 ? <div className={'text-center'}>There are no rules added, try adding one!</div> :
         <div className={'flex justify-center items-center w-full my-5'}>
-            <Paper sx={{ height: 400, width: '100%' }}>
+            <Paper sx={{ height: "500px", width: '80%'}}>
                 <DataGrid
+                    disableRowSelectionOnClick
                     rows={rules.map((rule, id) => {
                         return {...rule, id}
                     })}
+                    sx={{
+                        '& .MuiDataGrid-cell': {
+                            alignItems: 'start',
+                            whiteSpace: 'pre-wrap',
+                            paddingTop: '12px',
+                            paddingBottom: '12px',
+                        },
+                    }}
+                    getRowHeight={() => 'auto'}
                     checkboxSelection={false}
                     autoPageSize={true}
-                    columns={[{field: 'method', width: 300}, {field: 'path', width: 300}, {field: 'code', width: 300}]}
+                    columns={[{field: 'method',headerName: 'Method' }, {field: 'path', headerName: 'Path'}, {field: 'code', headerName:'Status Code',  width: 300}, {
+                        field: 'response',
+                        headerName: 'Response JSON',
+                        width: 200,
+                        flex: 1,
+
+                        renderCell: (params:GridRenderCellParams) => {
+                            return <div><JsonEditor rootName={''} viewOnly={true} data={JSON.parse(params.value)}></JsonEditor></div>
+                        }
+                    }]}
                 />
             </Paper>
         </div>
