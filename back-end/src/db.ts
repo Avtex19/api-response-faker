6e0f813d2aba763726ff0d9cb78a89235d6c9312
Avtandil Beradze <atukaberadze19@gmail.com>
2025-06-19 17:13:52 +0400

    update db path

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,9 +1,8 @@
-// db.ts
 import Database from 'better-sqlite3';
-import path from 'path';
+import * as path from "node:path";
 
-const dbPath = path.resolve(__dirname, "../db/response-faker.db");
+
+const dbPath = path.resolve('./db/response-faker.db');
 const db = new Database(dbPath);
-
 
 export default db;
